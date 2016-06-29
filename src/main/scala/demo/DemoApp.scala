effcb224e7bd85994e1bc9da189a08303a2d48c8
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2016-06-29 13:15:14 -0400

    Convert to the latest snapshot changes

--- src/main/scala/demo/DemoApp.scala
+++ src/main/scala/demo/DemoApp.scala
@@ -5,20 +5,19 @@ import java.util.Locale
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExport
-//import org.querki.jquery.$
 
 import scala.scalajs.locale.LocaleRegistry
-import scala.scalajs.locale.ldml.data.all._
+import scala.scalajs.locale.cldr.data.fi_FI
 
 @JSExport("DemoApp")
 object DemoApp extends js.JSApp {
   @scala.scalajs.js.annotation.JSExport
   override def main(): Unit = {
     // Minimal demo used for sanity checks and for size improvements
-    LocaleRegistry.installLocale(ar)
-    val tag = ar.languageTag
+    LocaleRegistry.installLocale(fi_FI)
+    val tag = fi_FI.languageTag
     val dfs = DecimalFormatSymbols.getInstance(Locale.forLanguageTag(tag))
-    println(ar.languageTag)
+    println(fi_FI.languageTag)
     println("NaN: " + dfs.getNaN)
   }
 }
